64b47233501d06b916b40b73e529d90654b336f9
21734327 <49047522+21734327@users.noreply.github.com>
2019-10-09 18:03:41 -0500

    Update midterm.ps1

--- midterm.ps1
+++ midterm.ps1
@@ -1,18 +1,37 @@
-$targetmachines = get-content c:\scripts\midterm_target_machines.txt
 $outpath = 'c:\scripts\midtermreports'
-
+$outfile = $outpath + '\' + $(Hostname) + '-INV-' + (get-date -format 'MM-dd-yyyy') + '.txt'
 if (!(test-path $outpath)) { mkdir $outpath }
 
-
-$targetmachines | foreach {
-  $outfile = $outpath + '\' + $_ + '-INV-' + (get-date -format 'MM-dd-yyyy') + '.txt'
-
-
   #write out name of machine
+'/*** Operating System Information ***/' | out-file $outfile
+'Hostname: ' + $(hostname) | out-file $outfile -append
+
+  
+  
   #write out os version
-  #write out processor information
-  #write out pysical memory
+$opsys = get-ciminstance win32_operatingsystem
+'OS Version: ' + $opsys.version | outfile $outfile -append
+"`n" | out-file $outfile -append
+
+#write out hardware information
+'/*** Hardware Information ***/' | out-file $outfile -append
+$processor = get-wmiobject win32_processor
+'Processor: ' + $processor.name | out-file $outfile -append
+'Processor manufacturer: ' + $processor.manufacturer | out-file $outfile -append
+'Processor core count: ' + $processor.numberofcores | out-file $outfile -append
+'Physical memory: ' + (Get-WMIObject -class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | foreach {[Math]::Round(($_.sum / 1GB),2)}) + 'GB' | out-file $outfile -append
+"`n" | out-file $outfile -append
+
   #write out roles and features
+if ($opsys.producttype -eq 1) {
+  '/*** Roles and Features are not available on a workstation ***/' | out-file $outfile -append
+} else {
+  '/*** Installed Roles and Features ***/' | out-file $outfile -append
+  get-windowsfeature | where installed | ft -autosize | out-file $outfile -append
+}
+
+
+  
   #write out whether remote desktp is enabled
   
   $logs = System,Application
